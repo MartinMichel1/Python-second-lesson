60dac69d8434cf70d5d7a4ab672dee72e3c87f16
Martin Michel Majcenovic <majcenovic.martin@gmail.com>
2020-04-29 14:53:39 +0200

    Correcting variable names

--- joining-strings.py
+++ joining-strings.py
@@ -2,7 +2,7 @@ import time
 print("I will ask 2 questions and then I will tell you something very obvious.")
 name = input("Tell me your name: ")
 age = input("How old are you?")
-sNinja = "and you're learning the basics of front-end and back-end at SmartNinja."
-print(f"Your name is {name}, you are {age} years old {sNinja}")
+smart_ninja = "and you're learning the basics of front-end and back-end at SmartNinja."
+print(f"Your name is {name}, you are {age} years old {smart_ninja}")
 
 time.sleep(5)
